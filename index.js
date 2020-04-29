c2804ecda61052701bba36fb9556f866332d21ad
‘S.M.Mehrabul <mehrab.24csedu.001@gmail.com>
2020-04-29 17:50:11 +0600

    Basic Functionality: Converting HTML file to PDF

---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs-extra');
+const prompt = require('prompt');
+
+(async () => {
+    try{
+        var filePath;
+        // For Taking File Path from CMD Prompt
+        // var prompt_attributes = [{name: 'filePath'}];
+        // prompt.start();
+        // prompt.get(prompt_attributes, (err, result) => {
+        //     if (err) {
+        //         console.log(err);
+        //     }else {
+        //         filePath = result.filePath;
+        //         toPDF(filePath);
+        //     }
+        // });
+
+        filePath = '.input/input.html'
+        toPDF(filePath);
+    }catch(e){
+        console.log(e);
+    }
+})();
+
+async function toPDF(filePath){
+        var inputHtml;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        inputHtml = fs.readFileSync(filePath, 'utf8');
+        await page.setContent(inputHtml);
+        await page.emulateMedia('screen');
+        await page.pdf({
+            path: './output/output.pdf',
+            format: 'A4',
+            printBackground: true
+        });
+
+        console.log('Successfully Coverted!');
+        await browser.close();
+        process.exit();
+}
